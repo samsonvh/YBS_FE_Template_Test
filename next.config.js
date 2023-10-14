f71fd299df8e3b324d281bfaf063b7ef9ded12cf
SamsonVH <samson.vh.20@gmail.com>
2023-10-14 18:22:58 +0700

    Add config for prettier and env

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    env: {
+        SERVER: process.env.BACKEND_HOST + process.env.API_V1,
+    }
+}
 
 module.exports = nextConfig
