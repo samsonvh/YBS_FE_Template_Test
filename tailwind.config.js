b1597d28682ddd0084f899cf8fece16ecaf918e3
SamsonVH <samson.vh.20@gmail.com>
2023-10-14 17:56:43 +0700

    Add tailwind, prettier

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  important: true,
+  prefix: 'tw-',
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
